92959623d28b49905d646cd341e5bdd0aed879a4
codinghabb <ademolaabds@gmail.com>
2023-12-02 11:29:13 +0100

    done

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,23 +1,11 @@
-import React from 'react';
+import React from 'react'
 
-export default function NotFound() {
-    const capitalize = () => {
-        const h2Elements = document.getElementsByTagName('h2');
-        for (let i = 0; i < h2Elements.length; i++) {
-            h2Elements[i].innerText = h2Elements[i].innerText.toUpperCase();
-        }
-    };
-
-    // Call the capitalize function when the component is mounted
-    React.useEffect(() => {
-        capitalize();
-    }, []);
-
-    return (
-        <div>
-            <h2 className='text-center text-3xl my-20'>
-                The link is not broken. Please reach out to us at ayodeleomojolag@gmail.com
-            </h2>
-        </div>
-    );
+export default function notFound() {
+ 
+  return (
+    <div>
+        
+        <h2 className='text-center text-3xl my-20'>the link is not broke it&apos;s please reach out to us at ayodeleomojolag@gmail,com</h2>
+    </div>
+  )
 }
